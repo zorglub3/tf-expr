8da8f84402bf02822efa135e26ba70fa93e291d5
Thomas Pecseli <pecseli@gmail.com>
2025-01-20 23:12:06 +0000

    Fix the XOR example. It works, but it learns very slow

--- examples/xor.rs
+++ examples/xor.rs
@@ -3,6 +3,9 @@ use tf_expr::expr::*;
 use tf_expr::*;
 use tf_expr::tensordata::TensorData;
 
+const HIDDEN_SIZE: usize = 8;
+const TRAINING_ITERATIONS: usize = 100_000;
+
 pub fn main() {
     let input_shape: [usize; 2] = [2, 1];
     let label_shape: [usize; 2] = [1, 1];
@@ -10,8 +13,8 @@ pub fn main() {
     let input = float_feed("input", &input_shape);
     let label = float_feed("label", &label_shape);
 
-    let weight1_shape: [usize; 2] = [3, 2];
-    let weight2_shape: [usize; 2] = [1, 3];
+    let weight1_shape: [usize; 2] = [HIDDEN_SIZE, 2];
+    let weight2_shape: [usize; 2] = [1, HIDDEN_SIZE];
 
     let w1_init = random_standard_normal(weight1_shape);
     let weight1 = float_variable(
@@ -28,13 +31,12 @@ pub fn main() {
     );
 
     let output = weight2.read().mat_mul(weight1.read().mat_mul(input.read()).tanh()).tanh();
-    let error  = (output.clone() - label.read()) * (output.clone() - label.read());
+    let error = output.clone() - label.read();
+    let error_sqr = error.clone() * error;
 
     let mut compiler = Compiler::new_with_root_scope();
 
-    // let error_op = compiler.get_operation(&error).unwrap();
-
-    let min_error = error.clone().minimize(&[weight1.refer(), weight2.refer()]);
+    let min_error = error_sqr.clone().minimize(&[weight2.refer(), weight1.refer()]);
 
     let _ = compiler.compile(&min_error).unwrap();
 
@@ -46,30 +48,38 @@ pub fn main() {
 
     println!("running training iterations");
 
-    for i in 0 .. 10000 {
-        let a = i & 1;
-        let b = (i & 2) >> 1;
-        let l = a ^ b;
+    for j in 0 .. TRAINING_ITERATIONS {
+        let mut error_sum: f32 = 0.;
 
-        let input_feed = TensorData::<2, FloatData<2>>::new(&input_shape, &[a as f32, b as f32]);
-        let label_feed = TensorData::<2, FloatData<2>>::new(&label_shape, &[l as f32]);
+        for i in 0 .. 4 {
+            let a = i & 1;
+            let b = (i & 2) >> 1;
+            let l = a ^ b;
 
-        let input_tensor = input_feed.tag().unwrap();
-        let label_tensor = label_feed.tag().unwrap();
+            let input_feed = TensorData::<2, FloatData<2>>::new(&input_shape, &[a as f32, b as f32]);
+            let label_feed = TensorData::<2, FloatData<2>>::new(&label_shape, &[l as f32]);
 
-        let mut args = session.session_run_args();
+            let input_tensor = input_feed.tag().unwrap();
+            let label_tensor = label_feed.tag().unwrap();
 
-        session.add_feed(&mut args, &input.refer(), &input_tensor).expect("Could not add input feed");
-        session.add_feed(&mut args, &label.refer(), &label_tensor).expect("Could not add label feed");
-        session.add_target(&mut args, &min_error);
+            let mut args = session.session_run_args();
 
-        let err_token = session.request_fetch(&mut args, &error).unwrap();
+            session.add_feed(&mut args, &input.refer(), &input_tensor).expect("Could not add input feed");
+            session.add_feed(&mut args, &label.refer(), &label_tensor).expect("Could not add label feed");
+            session.add_target(&mut args, &min_error);
 
-        session.run(&mut args).expect("Error running training iteration");
+            let err_token = session.request_fetch(&mut args, &error_sqr).unwrap();
 
-        let output = args.fetch::<f32>(err_token);
+            session.run(&mut args).expect("Error running training iteration");
 
-        println!("current error: {:?}", output);
+            let output = args.fetch::<f32>(err_token).unwrap();
+
+            error_sum += output[0].sqrt();
+        }
+
+        if j % 100 == 0 {
+            println!("Iteration {}. Error: {}", j, error_sum);
+        }
     }
 
     println!("All done");
